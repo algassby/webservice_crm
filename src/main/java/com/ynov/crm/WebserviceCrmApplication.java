665254a01776603dc805ce75c2e0326ee58f2a12
singarde <mathieugonza3@gmail.com>
2022-02-24 11:35:19 +0100

    classe administrateur

--- src/main/java/com/ynov/crm/WebserviceCrmApplication.java
+++ src/main/java/com/ynov/crm/WebserviceCrmApplication.java
@@ -8,6 +8,7 @@ public class WebserviceCrmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceCrmApplication.class, args);
+		
 	}
 
 }
---
+++ src/main/java/com/ynov/crm/enties/Administrateur.java
@@ -0,0 +1,13 @@
+package com.ynov.crm.enties;
+
+import lombok.Data;
+
+@Data
+public class Administrateur {
+
+	private String adminID;
+	private String firstName;
+	private String lastName;
+	private String eMailAddress;
+	
+}
