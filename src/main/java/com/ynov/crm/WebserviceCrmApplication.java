08d25dd457428f074c818cc11d8edd52797fb066
algassby <algassby@hotmail.com>
2022-02-24 10:23:49 +0100

    init project

---
+++ src/main/java/com/ynov/crm/WebserviceCrmApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceCrmApplication.class, args);
+	}
+
+}
